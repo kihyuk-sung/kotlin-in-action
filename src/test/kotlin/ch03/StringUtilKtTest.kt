b11c50686b1e975ce6f2b5395bea2f600eaaede1
K <kihyuk.sung@outlook.kr>
2022-03-26 22:19:08 +0900

    joinToString implementation

---
+++ src/test/kotlin/ch03/StringUtilKtTest.kt
@@ -0,0 +1,14 @@
+package ch03
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class StringUtilKtTest {
+    @Test
+    fun joinToString() {
+        val list = listOf(1, 2, 3)
+
+        assertThat(joinToString(list, "; ", "(", ")"))
+            .isEqualTo("(1; 2; 3)")
+    }
+}
