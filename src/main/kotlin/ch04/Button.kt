e0c7681215a0a6c39ace2835d7615b38bd3f31af
K <kihyuk.sung@outlook.kr>
2022-03-28 20:24:01 +0900

    인터페이스와 클래스 구현
    - 같은 표현을 갖는 메서드를 동시에 구현하는 경우, 반드시 override를 해야한다.

---
+++ src/main/kotlin/ch04/Button.kt
@@ -0,0 +1,10 @@
+package ch04
+
+class Button: Clickable, Focusable {
+    override fun click() = println("I was clicked")
+
+    override fun showOff() {
+        super<Clickable>.showOff()
+        super<Focusable>.showOff()
+    }
+}
